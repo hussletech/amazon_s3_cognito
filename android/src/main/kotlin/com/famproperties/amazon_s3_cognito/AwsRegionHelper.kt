4df33902598687c3f07a088a6c65f05803a6c105
hieuht2 <hieuht2@fsoft.com.vn>
2022-10-21 22:50:15 +0700

    Fixed S3 Cognito

--- android/src/main/kotlin/com/famproperties/amazon_s3_cognito/AwsRegionHelper.kt
+++ android/src/main/kotlin/com/famproperties/amazon_s3_cognito/AwsRegionHelper.kt
@@ -1,90 +1,121 @@
 package com.famproperties.amazon_s3_cognito
 
 import android.content.Context
+import android.os.AsyncTask
 import android.util.Log
-
-
 import com.amazonaws.auth.CognitoCachingCredentialsProvider
+import com.amazonaws.mobile.config.AWSConfiguration
 import com.amazonaws.mobileconnectors.s3.transferutility.*
+import com.amazonaws.regions.Region
 import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.S3ObjectSummary
 import java.io.File
 import java.io.UnsupportedEncodingException
 
 
-class AwsRegionHelper(private val context: Context, private val onUploadCompleteListener: OnUploadCompleteListener,
+class AwsRegionHelper(private val context: Context,
                       private val BUCKET_NAME: String, private val IDENTITY_POOL_ID: String,
-                      private val IMAGE_NAME: String,private val REGION: String, private val SUB_REGION: String) {
+                      private val REGION: String, private val SUB_REGION: String) {
 
     private var transferUtility: TransferUtility
-    private var nameOfUploadedFile: String? = null
     private var region1:Regions = Regions.DEFAULT_REGION
     private var subRegion1:Regions = Regions.DEFAULT_REGION
 
 
     init {
-
         initRegion()
-        val credentialsProvider = CognitoCachingCredentialsProvider(context, IDENTITY_POOL_ID, region1)
-        val amazonS3Client = AmazonS3Client(credentialsProvider)
-        amazonS3Client.setRegion(com.amazonaws.regions.Region.getRegion(subRegion1))
-        TransferNetworkLossHandler.getInstance(context.applicationContext)
-        transferUtility = TransferUtility(amazonS3Client, context)
+        transferUtility =  getTransferUtility()
+
     }
 
-    private val uploadedUrl: String
-        get() = getUploadedUrl(nameOfUploadedFile)
+    private fun getTransferUtility():TransferUtility{
+        //val awsConfiguration = AWSConfiguration(context)
+        //awsConfiguration.
+        val amazonS3Client = getAmazonS3Client()
+
+        TransferNetworkLossHandler.getInstance(context.applicationContext)
+        val transferOptions = TransferUtilityOptions()
+        transferOptions.transferThreadPoolSize = 18
+
+        return  TransferUtility.builder()
+                .s3Client(amazonS3Client).
+                transferUtilityOptions(transferOptions)
+                .defaultBucket(BUCKET_NAME)
+                .context(context)
+                .build()
+    }
+
 
     private fun getUploadedUrl(key: String?): String {
         return "https://s3-"+subRegion1.getName()+".amazonaws.com/"+BUCKET_NAME+"/"+key
         //return  ""+key
     }
 
+    private fun getAmazonS3Client():AmazonS3Client{
+        val credentialsProvider = CognitoCachingCredentialsProvider(context, IDENTITY_POOL_ID, region1)
+        return AmazonS3Client(credentialsProvider, Region.getRegion(subRegion1))
+
+    }
+
     private fun initRegion(){
 
-        region1 = getRegionFor(REGION)
-        subRegion1 = getRegionFor(SUB_REGION)
+        region1 = RegionHelper(REGION).getRegionName()
+        subRegion1 = RegionHelper(SUB_REGION).getRegionName()
 
     }
 
     @Throws(UnsupportedEncodingException::class)
-    fun deleteImage(): String {
+    fun deleteImage(imageName: String,folderToUploadTo: String?, onUploadCompleteListener: OnUploadCompleteListener): String {
 
         initRegion()
 
-        val credentialsProvider = CognitoCachingCredentialsProvider(context, IDENTITY_POOL_ID, region1)
         TransferNetworkLossHandler.getInstance(context.applicationContext)
 
-        val amazonS3Client = AmazonS3Client(credentialsProvider)
-        amazonS3Client.setRegion(com.amazonaws.regions.Region.getRegion(subRegion1))
-        Thread(Runnable{
-            amazonS3Client.deleteObject(BUCKET_NAME, IMAGE_NAME)
+        var key = imageName
+
+        if(folderToUploadTo != null){
+            key = if(folderToUploadTo.endsWith("/")){
+                folderToUploadTo + imageName
+            }else{
+                "$folderToUploadTo/$imageName"
+            }
+
+        }
+
+        val amazonS3Client =  getAmazonS3Client()
+        Thread( Runnable{
+            amazonS3Client.deleteObject(BUCKET_NAME, key)
         }).start()
         onUploadCompleteListener.onUploadComplete("Success")
-        return IMAGE_NAME
+        return imageName
 
     }
 
     @Throws(UnsupportedEncodingException::class)
-    fun uploadImage(image: File): String {
+    fun uploadImage(image: File, imageName: String,folderToUploadTo:String? ,onUploadCompleteListener: OnUploadCompleteListener): String {
 
         initRegion()
 
-        val credentialsProvider = CognitoCachingCredentialsProvider(context, IDENTITY_POOL_ID, region1)
         TransferNetworkLossHandler.getInstance(context.applicationContext)
 
-        val amazonS3Client = AmazonS3Client(credentialsProvider)
-        amazonS3Client.setRegion(com.amazonaws.regions.Region.getRegion(subRegion1))
-        transferUtility = TransferUtility(amazonS3Client, context)
+        var key = imageName
 
-        nameOfUploadedFile = IMAGE_NAME;
+        if(folderToUploadTo != null){
+            key = if(folderToUploadTo.endsWith("/")){
+                folderToUploadTo + imageName
+            }else{
+                "$folderToUploadTo/$imageName"
+            }
 
-        val transferObserver = transferUtility.upload(BUCKET_NAME, nameOfUploadedFile, image)
+        }
+
+        val transferObserver = transferUtility.upload(BUCKET_NAME, key, image)
 
         transferObserver.setTransferListener(object : TransferListener {
             override fun onStateChanged(id: Int, state: TransferState) {
                 if (state == TransferState.COMPLETED) {
-                    onUploadCompleteListener.onUploadComplete(getUploadedUrl(nameOfUploadedFile))
+                    onUploadCompleteListener.onUploadComplete(getUploadedUrl(key))
                 }
                 if (state == TransferState.FAILED ||  state == TransferState.WAITING_FOR_NETWORK) {
                     onUploadCompleteListener.onFailed()
@@ -98,76 +129,24 @@ class AwsRegionHelper(private val context: Context, private val onUploadComplete
 
             }
         })
-        return uploadedUrl
+        return getUploadedUrl(key)
     }
 
-    @Throws(UnsupportedEncodingException::class)
-    fun clean(filePath: String): String {
-        return filePath.replace("[^.A-Za-z0-9]".toRegex(), "")
-    }
 
     interface OnUploadCompleteListener {
         fun onUploadComplete(imageUrl: String)
         fun onFailed()
     }
 
+
+
     companion object {
         private val TAG = AwsRegionHelper::class.java.simpleName
         private const val URL_TEMPLATE = "https://s3.amazonaws.com/%s/%s"
     }
 
-    private fun  getRegionFor(name:String):Regions{
 
-        if(name == "US_EAST_1"){
-            return Regions.US_EAST_1
-        }else if(name == "AP_SOUTHEAST_1"){
-            return Regions.AP_SOUTHEAST_1
-        }else if(name == "US_EAST_2"){
-            return Regions.US_EAST_2
-        }else if(name == "EU_WEST_1"){
-            return Regions.EU_WEST_1
-        }else if(name == "CA_CENTRAL_1"){
-            return Regions.CA_CENTRAL_1
-        }else if(name == "CN_NORTH_1"){
-            return Regions.CN_NORTH_1
-        } else if(name == "CN_NORTHWEST_1"){
-            return Regions.CN_NORTHWEST_1
-        }else if(name == "EU_CENTRAL_1"){
-            return Regions.EU_CENTRAL_1
-        } else if(name == "EU_WEST_2"){
-            return Regions.EU_WEST_2
-        }else if(name == "EU_WEST_3"){
-            return Regions.EU_WEST_3
-        } else if(name == "SA_EAST_1"){
-            return Regions.SA_EAST_1
-        } else if(name == "US_WEST_1"){
-            return Regions.US_WEST_1
-        }else if(name == "US_WEST_2"){
-            return Regions.US_WEST_2
-        } else if(name == "AP_NORTHEAST_1"){
-            return Regions.AP_NORTHEAST_1
-        } else if(name == "AP_NORTHEAST_2"){
-            return Regions.AP_NORTHEAST_2
-        } else if(name == "AP_SOUTHEAST_1"){
-            return Regions.AP_SOUTHEAST_1
-        }else if(name == "AP_SOUTHEAST_2"){
-            return Regions.AP_SOUTHEAST_2
-        } else if(name == "AP_SOUTH_1"){
-            return Regions.AP_SOUTH_1
-        }else if(name == "ME_SOUTH_1"){
-            return Regions.ME_SOUTH_1
-        }else if(name == "AP_EAST_1"){
-            return Regions.AP_EAST_1
-        }else if(name == "EU_NORTH_1"){
-            return Regions.EU_NORTH_1
-        }else if(name == "US_GOV_EAST_1"){
-            return Regions.US_GOV_EAST_1
-        }else if(name == "us-gov-west-1"){
-            return Regions.GovCloud
-        }
-
-        return Regions.DEFAULT_REGION
-
-    }
 
 }
+
+
